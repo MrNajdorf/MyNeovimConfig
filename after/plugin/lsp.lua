46db99354de72b187bb575a74953911ff5014d7b
MrNajdorf <210305105234@paruluniversity.ac.in>
2023-12-17 16:03:32 +0530

    added C language Server

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -21,6 +21,18 @@ nvim_lsp.emmet_ls.setup({
     }
 })
 
+-- C/C++ lsp settings
+nvim_lsp.clangd.setup({
+    on_attach = on_attach,
+    capabilities = capabilities,
+    cmd = { "clangd", "--background-index" },
+    filetypes = { "c", "cpp", "objc", "objcpp" },
+    init_options = {
+      clangdFileStatus = true
+    }
+})
+-- C/C++ lsp settings
+
 -- emmet lsp settings
 
 local on_attach = function(client,bufnr)
@@ -158,11 +170,11 @@ vim.api.nvim_create_autocmd('LspAttach', {
       -- vim.keymap.set('n', '<space>wl', function()
       --     print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
       -- end, opts)
-      vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
-      vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
-      vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
+      vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, opts)
+      vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
+      vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, opts)
       vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-      vim.keymap.set('n', '<space>pp', function()
+      vim.keymap.set('n', '<leader>pp', function()
           vim.lsp.buf.format { async = true }
       end, opts)
   end,
